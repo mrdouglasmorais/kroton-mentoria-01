b26b00bcf789084163b44e73f92387e1db59b2ee
Douglas Morais <mr.douglasmorais23@gmail.com>
2021-05-21 14:38:07 -0300

    Create element with JS

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 
 const form = `
   <div>
@@ -7,9 +7,38 @@ const form = `
       <input type="email" id="email" placeholder="informe seu email">
       <input type="submit" value="cadastrar">
     </form>
+    <button id="button">Limpar formulário</button>
+
+    <ul id="list"></ul>
   </div>
 `
 
 root.innerHTML = form;
 
-const formulario = document.getElementById('formulario')
+const formulario = document.getElementById('formulario');
+
+formulario.addEventListener('submit', function(e){
+  e.preventDefault();
+
+  function createInputs(){
+    document.getElementById('name').value = ''
+    document.getElementById('email').value = ''
+  }
+
+  let name = document.getElementById('name').value
+  let email = document.getElementById('email').value
+
+  let list = document.getElementById('list')
+  let li = document.createElement('li')
+  li.append(`${name} | ${email}`)
+  list.append(li)
+
+  return createInputs(); 
+})
+
+const button = document.getElementById('button');
+
+button.addEventListener('click', function(){
+  document.getElementById('name').value = ''
+  document.getElementById('email').value = ''
+})
